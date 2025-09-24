062808f84b7846d8c0e287ed49772e57615ad2c2
Kev_CB <kevcb369@gmail.com>
2025-09-24 12:27:10 -0500

    Validación usuario registrado - VeterinarioController

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,21 +1,22 @@
 import Veterinario from "../models/Veterinario.js"
 
 const registrar = async (req, res) => {
+    const { email } = req.body;
+    // Pevenir registro duplicado
+    const usuarioExistente = await Veterinario.findOne({ email: email })
+    if (usuarioExistente) {
+        const error = new Error('Usuario ya registrado.')
+        return res.status(400).json({ msg: error.message })
+    }
+
     try {
+        // Guardar nuevo veterninario
         const veterinario = new Veterinario(req.body)
         const vaterinarioGuardado = await veterinario.save()
         res.json(vaterinarioGuardado)
     } catch (error) {
         console.error(error)
     }
-
-
-    // const { nombre, email, password } = req.body
-
-    // console.log("****** REGISTRO ********")
-    // console.log("Nombre: ", nombre)
-    // console.log("E-mail: ", email)
-    // console.log("Pass: ", password)
 }
 
 const perfil = (req, res) => {
