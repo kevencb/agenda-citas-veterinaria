a179f4dc46aafa8df39360049f3e74a47f08a247
Kev_CB <kevcb369@gmail.com>
2025-09-24 15:22:30 -0500

    Validación usuario registrado - Hasheo de password & Validación de cuenta

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,19 +1,24 @@
+import { request } from "express";
 import Veterinario from "../models/Veterinario.js"
 
 const registrar = async (req, res) => {
+
     const { email } = req.body;
-    // Pevenir registro duplicado
+
+    /**** Inicio: Prevenir registro duplicado ****/
     const usuarioExistente = await Veterinario.findOne({ email: email })
     if (usuarioExistente) {
         const error = new Error('Usuario ya registrado.')
         return res.status(400).json({ msg: error.message })
     }
+    /**** Fin: Pevenir registro duplicado ****/
 
     try {
-        // Guardar nuevo veterninario
+        /**** Inicio: Guardar nuevo veterninario ****/
         const veterinario = new Veterinario(req.body)
         const vaterinarioGuardado = await veterinario.save()
         res.json(vaterinarioGuardado)
+        /**** Fin: Guardar nuevo veterninario ****/
     } catch (error) {
         console.error(error)
     }
@@ -23,6 +28,29 @@ const perfil = (req, res) => {
     res.json({ msg: "Mostrando perfil" })
 }
 
+const confirmar = async (req, res) => {
+    // Cuando llenamos un form los datos los obtenemos mediante req.body, en caso parámetros se obtienen medianta res.params
+    const { token } = req.params
+
+    // findOne nos permite buscar entre las propiedades del objeto
+    const usuarioConfirmar = await Veterinario.findOne({ token })
+
+    if (!usuarioConfirmar) {
+        const error = new Error('Token no válido')
+        return res.status(404).json({ msg: error.message })
+    }
+
+    try {
+        usuarioConfirmar.token = null
+        usuarioConfirmar.confirmado = true
+        await usuarioConfirmar.save()
+
+        res.json({ msg: "Usuario confirmado correctamente." })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 export {
-    registrar, perfil
+    registrar, perfil, confirmar
 }
--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import generarId from "../helpers/generarId.js";
 
 const veterinarioSchema = mongoose.Schema({
     nombre: {
@@ -21,14 +23,30 @@ const veterinarioSchema = mongoose.Schema({
         default: null,
         trim: true
     },
+    web: {
+        type: String,
+        default: null,
+        trim: true
+    },
     token: {
-        type: String
+        type: String,
+        default: generarId
     },
     confirmado: {
         type: Boolean,
         default: false
     }
 });
+
+veterinarioSchema.pre('save', async function (next) {
+    // Si el password ya esta hasheao al momento de actualizar alguna u¡información el password no va a ser hasheado nuevamente
+    if (!this.isModified('password')) {
+        next() //Middleware de express
+    }
+    // genSalt: Son las rondas de hasheo
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
 export default Veterinario;
